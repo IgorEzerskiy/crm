f4ad1aabd74a07161d11d564c3bfac51a691c8a5
Dankir30 <kirrdann@gmail.com>
2023-08-09 20:50:56 +0300

    validated user creation form

--- crm_app/forms.py
+++ crm_app/forms.py
@@ -41,7 +41,21 @@ class UserCreateForm(UserCreationForm):
         self.fields['password2'].widget.attrs.update({'class': 'form-control'})
         self.fields['create_company'].widget.attrs.update({'class': 'form-check-input'})
 
-        #  TODO: Validate first_name, last_name, company, create_company.
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+
+        if not first_name.isalpha():
+            raise forms.ValidationError('Only letter')
+
+        return first_name
+
+    def clean_last_name(self):
+        last_name = self.cleaned_data.get('last_name')
+
+        if not last_name.isalpha():
+            raise forms.ValidationError('Only letter')
+
+        return last_name
 
 
 class UserLoginForm(AuthenticationForm):
